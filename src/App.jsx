93b5b54f17cd736cabf2728383a586840cdb78b2
tonytor <teme4842@gmail.com>
2023-10-19 09:51:37 +0300

    update the css file

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,26 @@ import axios from "axios";
 
 
 const App = () => {
-  const [searchInput, setsearchInput] = useState("");
-  const [defination, setdefination] = useState("");
+  const [searchInput, setsearchInput] = useState([]);
+  const [defination, setdefination] = useState({});
 
   const fetchDefination = async () => {
     const resp = await axios.get(`http://localhost:5000/${searchInput}`);
-    const word = resp.data?.meaning.split(' ')
-   // const original = word[0]
+   // const word = resp.data?.meaning.split(' ')
+  
   //  const def = word[1]
-   // console.log(resp.data);
-    setdefination(word);
+    console.log(resp.data);
+    setdefination(resp.data);
   };
 
   const handleClick = () => {
   console.log(defination);
     fetchDefination();
   };
+
+  // const Filter = (e)=>{
+  //   setdefination(searchInput.filter(f=>f.))
+  // }
 
   return (
     <div>
@@ -48,6 +52,7 @@ const App = () => {
 
       <div>
       
+   
 
         <p className="result">{defination[0]}</p>
          <p className="results">{defination}</p>
